084193b1e9dd4e26beff307ab23684c37d26107d
Jhorman <jhormandmartinezc@gmail.com>
2022-08-31 19:17:35 -0400

    Nuevos archivos

---
+++ Dividir.py
@@ -0,0 +1,7 @@
+variable1 = input('Introduce el primer numero: ')
+variable2 = input('Introduce el segundo numero: ')
+
+Division = int(variable1) / int(variable2) 
+
+print( 'El resultado de la Division es: ', Division ) 
+
---
+++ Multiplicar.py
@@ -0,0 +1,7 @@
+variable1 = input('Introduce el primer numero: ')
+variable2 = input('Introduce el segundo numero: ')
+
+Multiplicacion = int(variable1) * int(variable2) 
+
+print( 'El resultado de la Multiplicacion es: ', Multiplicacion ) 
+
