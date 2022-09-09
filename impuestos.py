943bade17b00edb1295abe253225784d7a3e6d38
Octavio Martinez <omartinez1618@gmail.com>
2022-09-09 19:05:36 -0400

    ejercicio de impuestos

---
+++ impuestos.py
@@ -0,0 +1,7 @@
+# Menos de 100€       	=>          5%
+# Entre 100€ y 200€	    =>          15%
+# Entre 200€ y 350€	    =>          20%
+# Entre 350€ y 600€	    =>          30%
+# Más de 600€	        =>          45%
+
+input( 'Cuál es el sueldo? ')
